baf7d8df9733d115928cff813c49801586ceb5a9
rockwyc992 <rockwyc992@gmail.com>
2017-04-05 00:27:40 +0800

    Create 4 basic models
    - Zone
    - Sub_Zone
    - Record_Type
    - Record

--- monkey_pdns/app/admin.py
+++ monkey_pdns/app/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Zone
+from .models import Sub_Zone
+from .models import Record
+from .models import Record_Type
+
+admin.site.register(Zone)
+admin.site.register(Sub_Zone)
+admin.site.register(Record)
+admin.site.register(Record_Type)
+
--- monkey_pdns/app/models.py
+++ monkey_pdns/app/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Zone(models.Model):
+    name = models.CharField(max_length = 64)
+    created = models.DateTimeField(auto_now_add = True)
+
+    def __str__(self):
+        return self.name
+
+class Sub_Zone(models.Model):
+    owner = models.ForeignKey(User)
+    prefix = models.CharField(max_length = 32)
+    super_zone = models.ForeignKey(Zone)
+    created = models.DateTimeField(auto_now_add = True)
+
+    def __str__(self):
+        return self.prefix + '.' + self.super_zone.name
+
+class Record_Type(models.Model):
+    name = models.CharField(max_length = 8)
+    created = models.DateTimeField(auto_now_add = True)
+
+    def __str__(self):
+        return self.name
+
+class Record(models.Model):
+    prefix = models.CharField(max_length = 32)
+    type = models.ForeignKey(Record_Type)
+    zone = models.ForeignKey(Sub_Zone)
+    context = models.CharField(max_length = 128)
+    created = models.DateTimeField(auto_now_add = True)
+
+    def __str__(self):
+        return self.prefix + '.' + str(self.zone) + ' ' + str(self.type) + ' ' + self.context
+
+
