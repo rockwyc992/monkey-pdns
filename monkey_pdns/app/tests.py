b041372b16faaec4cf88b4969493834d27ec388f
Chuanchan <williamtsai1111@gmail.com>
2017-04-05 00:54:51 +0800

    Fix tests.py For Issue #1
    Someone did his best job.

--- monkey_pdns/app/tests.py
+++ monkey_pdns/app/tests.py
@@ -7,5 +7,5 @@ class View_hello_tests(TestCase):
     def test_hello(self):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response, 'Hello, world.')
+        self.assertContains(response, 'Hello World!')
 
